00e4d00cca1a39be21964fdbec0f3c62f8054eb1
Traines <76391536+traines-source@users.noreply.github.com>
2021-01-04 19:07:29 +0000

    switch to conjugate gradient instead of nelder mead, refactoring

--- src/Gravitator.ts
+++ src/Gravitator.ts
@@ -1,4 +1,4 @@
-import { Station } from "./Station";
+import { Station, Stop } from "./Station";
 import { Vector } from "./Vector";
 import { Line } from "./Line";
 import { Utils } from "./Utils";
@@ -8,8 +8,9 @@ const fmin = require('fmin');
 
 
 export class Gravitator {
-    static INERTNESS = 5;
-    static DEVIATION_WARNING = 0.1;
+    static INERTNESS = 10;
+    static GRADIENT_SCALE = 0.000000001;
+    static DEVIATION_WARNING = 0.05;
 
     private initialWeightFactors: {[id: string] : number} = {};
     private edges: {[id: string]: Line} = {};
@@ -21,44 +22,13 @@ export class Gravitator {
 
     gravitate() {
         this.obtainStations();
-        const initial: number[] = [];
-        for (const vertex of Object.values(this.vertices)) {
-            initial[vertex.index.x] = vertex.station.baseCoords.x;
-            initial[vertex.index.y] = vertex.station.baseCoords.y;            
-        }
-        const gravitator = this;
-        const solution = fmin.nelderMead((X: number[]) => {
-            let value = 0;
-            for (const vertex of Object.values(gravitator.vertices)) {
-                value += (
-                        Math.pow(X[vertex.index.x]-vertex.station.baseCoords.x, 2) +
-                        Math.pow(X[vertex.index.y]-vertex.station.baseCoords.y, 2)
-                    ) / Gravitator.INERTNESS;
-            }
-            for (const [key, edge] of Object.entries(gravitator.edges)) {
-                value += Math.pow(
-                    Math.pow(X[gravitator.vertices[edge.termini[0].stationId].index.x] - X[gravitator.vertices[edge.termini[1].stationId].index.x], 2) +
-                    Math.pow(X[gravitator.vertices[edge.termini[0].stationId].index.y] - X[gravitator.vertices[edge.termini[1].stationId].index.y], 2) -
-                    Math.pow(gravitator.initialWeightFactors[key] * (edge.length || 0), 2),
-                2);
-            }
-            return value;
-        }, initial);
-        console.log('solution:', solution.x);
+        const solution = this.minimizeLoss();
+        console.log('solution:', solution);
         console.log(this.vertices);
         console.log(this.initialWeightFactors);
         console.log(this.edges);
-        for (const [key, edge] of Object.entries(gravitator.edges)) {
-            if (Math.abs(1 - Math.sqrt(
-                Math.pow(solution.x[gravitator.vertices[edge.termini[0].stationId].index.x] - solution.x[gravitator.vertices[edge.termini[1].stationId].index.x], 2) +
-                Math.pow(solution.x[gravitator.vertices[edge.termini[0].stationId].index.y] - solution.x[gravitator.vertices[edge.termini[1].stationId].index.y], 2)
-            ) / (gravitator.initialWeightFactors[key] * (edge.length || 0))) > Gravitator.DEVIATION_WARNING) {
-                console.warn(edge.name, 'diverges by more than', Gravitator.DEVIATION_WARNING )
-            }
-        }
-        for (const vertex of Object.values(this.vertices)) {
-            vertex.station.baseCoords = new Vector(solution.x[vertex.index.x], solution.x[vertex.index.y]);
-        }
+        this.assertDistances(solution);
+        this.updateStations(solution);
     }
 
     private obtainStations() {
@@ -74,6 +44,87 @@ export class Gravitator {
         for (const vertex of Object.values(this.vertices)) {
             vertex.index = new Vector(i, i+1);
             i += 2;
+        }
+    }
+
+    private minimizeLoss(): number[] {
+        const gravitator = this;
+        const params = {history: []};
+        const initial: number[] = this.initializeWithPreviousStationPositions();
+        const solution = fmin.conjugateGradient((A: number[], fxprime: number[]) => {
+            fxprime = fxprime || A.slice();
+            let fx = 0;
+            fx = this.deltaToPreviousStationPositionsToEnsureInertness(fx, A, fxprime, gravitator);
+            fx = this.deltaToNewDistancesToEnsureAccuracy(fx, A, fxprime, gravitator);
+            this.scaleGradientToEnsureWorkingStepSize(fxprime);
+            return fx;
+        }, initial, params);
+        return solution.x;
+    }
+
+    private initializeWithPreviousStationPositions(): number[] {
+        const initial: number[] = [];
+        for (const vertex of Object.values(this.vertices)) {
+            initial[vertex.index.x] = vertex.station.baseCoords.x;
+            initial[vertex.index.y] = vertex.station.baseCoords.y;
+        }
+        return initial;
+    }
+
+    private deltaX(A: number[], vertices: {[id: string] : {station: Station, index: Vector}}, termini: Stop[]): number {
+        return A[vertices[termini[0].stationId].index.x] - A[vertices[termini[1].stationId].index.x];
+    }
+
+    private deltaY(A: number[], vertices: {[id: string] : {station: Station, index: Vector}}, termini: Stop[]): number {
+        return A[vertices[termini[0].stationId].index.y] - A[vertices[termini[1].stationId].index.y];
+    }
+
+    private deltaToPreviousStationPositionsToEnsureInertness(fx: number, A: number[], fxprime: number[], gravitator: Gravitator): number {
+        for (const vertex of Object.values(gravitator.vertices)) {
+            fx += (
+                    Math.pow(A[vertex.index.x]-vertex.station.baseCoords.x, 2) +
+                    Math.pow(A[vertex.index.y]-vertex.station.baseCoords.y, 2)
+                ) * Gravitator.INERTNESS;
+            fxprime[vertex.index.x] = 2 * (A[vertex.index.x]-vertex.station.baseCoords.x) * Gravitator.INERTNESS;
+            fxprime[vertex.index.y] = 2 * (A[vertex.index.y]-vertex.station.baseCoords.y) * Gravitator.INERTNESS;
+        }
+        return fx;
+    }
+
+    private deltaToNewDistancesToEnsureAccuracy(fx: number, A: number[], fxprime: number[], gravitator: Gravitator): number {
+        for (const [key, edge] of Object.entries(gravitator.edges)) {                
+            const v = Math.pow(this.deltaX(A, gravitator.vertices, edge.termini), 2)
+                        + Math.pow(this.deltaY(A, gravitator.vertices, edge.termini), 2)
+                        - Math.pow(gravitator.initialWeightFactors[key] * (edge.length || 0), 2);
+            fx += Math.pow(v, 2);
+            fxprime[gravitator.vertices[edge.termini[0].stationId].index.x] += +4 * v * this.deltaX(A, gravitator.vertices, edge.termini);
+            fxprime[gravitator.vertices[edge.termini[0].stationId].index.y] += +4 * v * this.deltaY(A, gravitator.vertices, edge.termini);
+            fxprime[gravitator.vertices[edge.termini[1].stationId].index.x] += -4 * v * this.deltaX(A, gravitator.vertices, edge.termini);
+            fxprime[gravitator.vertices[edge.termini[1].stationId].index.y] += -4 * v * this.deltaY(A, gravitator.vertices, edge.termini);
+        }
+        return fx;
+    }
+
+    private scaleGradientToEnsureWorkingStepSize(fxprime: number[]): void {
+        for (let i=0; i<fxprime.length; i++) {
+            fxprime[i] *= Gravitator.GRADIENT_SCALE;
+        }
+    }
+
+    private assertDistances(solution: number[]) {
+        for (const [key, edge] of Object.entries(this.edges)) {
+            if (Math.abs(1 - Math.sqrt(
+                Math.pow(this.deltaX(solution, this.vertices, edge.termini), 2) +
+                Math.pow(this.deltaY(solution, this.vertices, edge.termini), 2)
+            ) / (this.initialWeightFactors[key] * (edge.length || 0))) > Gravitator.DEVIATION_WARNING) {
+                console.warn(edge.name, 'diverges by more than', Gravitator.DEVIATION_WARNING )
+            }
+        }
+    } 
+
+    private updateStations(solution: number[]) {
+        for (const vertex of Object.values(this.vertices)) {
+            vertex.station.baseCoords = new Vector(solution[vertex.index.x], solution[vertex.index.y]);
         }
     }
 
